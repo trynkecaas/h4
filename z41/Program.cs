61a0d638edea640f0c5f9877257f1320a9fadb2c
trynkecaas <122730663+trynkecaas@users.noreply.github.com>
2023-03-18 14:59:17 +0300

    z41

---
+++ z41/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int Prompt (string message)
+{
+  System.Console.Write (message);
+  string value = Console.ReadLine ();
+  int result = Convert.ToInt32(value);
+  return result;
+}
+int[] InputArray (int length)
+{
+int [] array = new int [length];
+for (int i = 0; i < array.Length; i++)
+{
+  array [i] = Prompt($"Введите {i + 1}-й элемент ");
+  }
+return array;
+}
+void PrintArray (int [] array)
+{
+  for (int i = 0; i <array.Length; i++)
+  {
+    Console.WriteLine($"a[{i}] = {array[i]}");
+  }
+}
+int CountPositiveNumbers(int[]array)
+{
+  int count = 0;
+  for (int i =0; i <array.Length; i++)
+  {
+    if (array[i]> 0)
+    {
+      count++;
+    }
+  }
+
+return count;
+}
+int length = Prompt("Введите количество элементов >");
+int [] array;
+array = InputArray (length);
+PrintArray(array);
+Console.WriteLine($"Количество чисел больше 0 -{CountPositiveNumbers(array)}");
