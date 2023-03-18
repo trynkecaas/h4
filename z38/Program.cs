6a36e4c26196a87323318e139a03cfcf1cfb475c
trynkecaas <122730663+trynkecaas@users.noreply.github.com>
2023-03-18 13:03:15 +0300

    x38

---
+++ z38/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+double[] randomArray = new double[a];
+
+void mas(int a)
+{
+Random rand = new Random();
+for (int i = 0; i < a; i++)
+{
+randomArray[i] = rand.NextDouble();
+Console.WriteLine($"{randomArray[i]:F3} ");
+}
+
+}
+
+double raz(double[] randomArray)
+{
+double min = randomArray[0];
+double max = randomArray[0];
+int i = 1;
+while (i < randomArray.Length)
+{
+if (max<randomArray[i])
+max = randomArray[i];
+if (min>randomArray[i])
+min = randomArray[i];
+i = i + 1;
+}
+return max-min;
+}
+
+mas(a);
+Console.WriteLine($"Разница между максимальным и минимальным элементов массива: {raz(randomArray):F3}");
