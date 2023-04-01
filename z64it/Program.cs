d9ac2763d9a057e0d5c2a49523db287136ead6d9
trynkecaas <122730663+trynkecaas@users.noreply.github.com>
2023-04-01 11:29:29 +0300

    z66it

---
+++ z64it/Program.cs
@@ -0,0 +1,21 @@
+﻿    // Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+    int N = InputInt(" введите положительное число ");
+    int M = 1;
+    if (N < 1)
+    {
+      Console.WriteLine("введено не положительное число");
+    }
+    Console.WriteLine(NaturalNumber(N, M));
+    int NaturalNumber(int N, int M)
+    {
+      if (N == M)
+        return N;
+      else
+        Console.Write($"{NaturalNumber(N, M + 1)}, ");
+      return M;
+    }
+    int InputInt(string output)
+    {
+      Console.Write(output);
+      return int.Parse(Console.ReadLine());
+    }
---
+++ z66it/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int M = InputInt("Введите M:");
+int N = InputInt("Введите N:");
+Console.WriteLine($"Сумма элементов от {M} до {N} = {CountNaturalSum(M, N)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int CountNaturalSum(int M, int N)
+{
+    if (M == N)
+        return N;
+    return N + CountNaturalSum(M, N - 1);
+}
